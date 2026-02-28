04d4b85ef4a9790200364124ec94283cd0423e71
Ruan <lucasruanpracf@hotmail.com>
2026-02-28 18:41:11 -0300

    department findall

--- src/main/java/application/Program2.java
+++ src/main/java/application/Program2.java
@@ -4,8 +4,7 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program2 {
@@ -20,24 +19,29 @@ public class Program2 {
         Department department = departmentDao.findById(3);
         System.out.print(department);
 
-        System.out.println("=== TESTE 2: departmet delete ===");
+        System.out.println("=== TEST 2: department findall ===");
+        List<Department> list = departmentDao.findall();
+        for(Department obj : list){
+            System.out.print(obj);
+        }
+
+        System.out.println("=== TEST 3: department insert === ");
+        Department newDepartment = new Department(null, "Cars");
+        departmentDao.insert(newDepartment);
+        System.out.print("Inserted! New id = " + newDepartment.getId());
+
+        System.out.println("\n=== TEST 4: department update ===");
+        department = departmentDao.findById(16);
+        department.setName("Motors");
+        departmentDao.update(department);
+        System.out.println("Update completed");
+
+        System.out.println("=== TESTE 5: departmet delete ===");
         System.out.print("Enter id for delete test: ");
         int id = sc.nextInt();
         departmentDao.deleteById(id);
         System.out.print("Delete completed! ");
         System.out.println(">>> DELETE CHAMADO com id: " + id);
-
-        System.out.println("=== TEST 4: department insert === ");
-        Department newDepartment = new Department(null, "Cars");
-        departmentDao.insert(newDepartment);
-        System.out.print("Inserted! New id = " + newDepartment.getId());
-
-        System.out.println("\n=== TEST 5: department update ===");
-        department = departmentDao.findById(16);
-        department.setName("Motors");
-        departmentDao.update(department);
-        System.out.println("Update completed");
         }
-
     }
 
--- src/main/java/model/dao/impl/DepartmentDaoJDBC.java
+++ src/main/java/model/dao/impl/DepartmentDaoJDBC.java
@@ -6,6 +6,7 @@ import model.dao.DepartmentDao;
 import model.entities.Department;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -132,7 +133,32 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public List<Department> findall() {
-        return List.of();
+
+            PreparedStatement st = null;
+            ResultSet rs = null;
+
+            try {
+                st = conn.prepareStatement(
+                        "SELECT id, name FROM department"
+                );
+
+                rs = st.executeQuery();
+
+                List<Department> list = new ArrayList<>();
+
+                while (rs.next()) {
+                    list.add(instantiateDepartment(rs));
+                }
+
+                return list;
+
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            } finally {
+                DB.closeStatement(st);
+                DB.closeResultSet(rs);
+
+        }
     }
 
     private Department instantiateDepartment(ResultSet rs) throws SQLException {
