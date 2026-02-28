918626f8822e82a289040e4b6c1abb523cc26993
Ruan <lucasruanpracf@hotmail.com>
2026-02-28 01:07:37 -0300

    department update and findbyid

--- src/main/java/application/Program2.java
+++ src/main/java/application/Program2.java
@@ -10,10 +10,21 @@ public class Program2 {
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        System.out.print("=== TEST 1: department insert === ");
+        System.out.println("=== TEST 1: department findbyId ===");
+        Department department = departmentDao.findById(3);
+        System.out.print(department);
+
+        System.out.print("=== TEST 4: department insert === ");
         Department newDepartment = new Department(null, "Cars");
         departmentDao.insert(newDepartment);
         System.out.print("Inserted! New id = " + newDepartment.getId());
 
+        System.out.println("\n=== TEST 5: department update ===");
+        department = departmentDao.findById(8);
+        department.setName("Motors");
+        departmentDao.update(department);
+        System.out.println("Update completed");
+        }
+
     }
-}
+
